44399dedbfcd57c4f619d759268c800702527989
Laetitia Herbin-Collot <laetic@aol.com>
2019-02-14 14:04:01 +0100

    routes++

--- src/app/routes/app-routing.module.ts
+++ src/app/routes/app-routing.module.ts
@@ -2,14 +2,21 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
+import { LegalComponent } from './legal/legal.component';
+import { ListComponent } from './list/list.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
-const routes: Routes = [{
-  path: '', component: HomeComponent
-}];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'list', component: ListComponent},
+  {path: 'legal', component: LegalComponent},
+  {path: '**', component: NotfoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
-  declarations: [HomeComponent, ContactComponent]
+  declarations: [HomeComponent, ContactComponent, LegalComponent, ListComponent, NotfoundComponent]
 })
 export class AppRoutingModule { }
